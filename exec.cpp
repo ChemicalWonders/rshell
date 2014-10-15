2621dc78783bc7648c0dc20b780bd73245f13dea
Kevin Chan <kchan039@gmail.com>
2014-10-14 18:01:57 -0700

    completed some changes inside the files.; :

--- exec.cpp
+++ exec.cpp
@@ -8,15 +8,22 @@ using namespace std;
 
 int main()
 {
+	fork();
+	fork();
+	fork();
+	cout << "test" << endl;
+
+	
+	/*
 	char *argv[3];
 	argv[0] = new char[3];
 	strcpy(argv[0], "ls");
 	argv[1] = "-a";
-	argv[2] = "l";
+	argv[2] = "-l";
 	cout << "before" << endl;
 	execvp ("ls", argv);
 	cout << "after" << endl; // Nothing after execvp will ever be run.
-
+	*/
 	return 0;
 
 }
