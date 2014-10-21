3bfcff27b2b5b365468431e5d5f6af00171eeebe
Kevin Chan <kchan039@gmail.com>
2014-10-21 14:12:18 -0700

    Change some more files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <boost/tokenizer.hpp>
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -10,8 +9,10 @@ using namespace std;
 int main()
 {       
         string user_stream;
-        char * commands;
-        //char * username = getlogin();
+        const char * commands;
+        char * parsed;
+        const char * parser = " ;";
+        char * username = getlogin();
 
         char host[1024];
         gethostname(host, 1024);
@@ -19,42 +20,51 @@ int main()
         //Introductory Message to make sure shell is running.
         cout << "Hello, and welcome to Kevin's shell." << endl;
 
-        getline(cin, user_streaming);
-
-        user_stream = user_streaming.c_str();
-
-        tokenizer<> tok(user_stream);
-
-        for(tokenizer<>::iterator it=tok.begin(); it!=tok.end(); it++){
-                cout << *it << "\n";
-        }
-      /*
         while(1){
 
-                //int pid = fork();
                 // Prints out the username and hostname if it exists.
                 if (username && host){
                         cout << username << "@" << host;
                 }
-
                 cout << "$ ";
+
+                //Gets line for user stream.
                 getline(cin, user_stream);
+
+                //Parsing the string using strdup to change from const char* to char*
+                parsed = strdup(user_stream.c_str());
+
+                //Tokenizes the first one in the array.
+                commands = strtok(parsed, parser);
+
+                
+                /* walk through to check if commands are null terminated.
+                while( commands != NULL ) 
+                {
+                        printf( " %s\n", commands );
+                        commands = strtok(NULL, parser );
+                }
+                
 
                 if (user_stream == "exit"){
                         cout << "Shell will be terminated now." << endl; 
                         exit(0);
                 }
+                */
+
+                //Creating a fork process
+                int pid = fork();
         
 
-                //if (pid == 0){
-                //cout << "You have typed something in. What do I do now?" << endl;
-                //cout << "I'm a child process. " << endl;
-                //PID will return 0 when you have nothing left to execute.
+                if (pid == -1){
 
-
-                //}
+                        //PID will return -1 when an error occurs.
+                        cout << "Fork broke. Shell will now exit." << endl;
+                        exit(1);
+                }
 
         }
+        /*
 	if (pid == 0){
 		cout << "I'm a child process. " << endl;
 		//PID returns 0 when you have nothing left to execute.
