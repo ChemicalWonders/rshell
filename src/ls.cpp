37c8a5a0447a63c71b264426b9d61694c00bd8db
Kevin Chan <kchan039@gmail.com>
2014-11-11 10:46:37 -0800

    changed a file

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <sys/stat.h>
-#include <sys/type.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <errno.h>
+
+using namespace std;
 
 int main()
 {
     char *dirName = ".";
     DIR *dirp = opendir(dirName);
     dirent *direntp;
-    while ((direntp = readdir(dirp)))
+
+    while ((direntp = readdir(dirp)) != NULL)
         cout << direntp->d_name << endl;
         // use stat here to find attributes of file
     closedir(dirp);
