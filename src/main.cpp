7870bab5fea084b5d65649c7aae1729d30900d4c
Kevin Chan <kchan039@gmail.com>
2014-11-20 11:26:23 -0800

    "committed"

--- src/main.cpp
+++ src/main.cpp
@@ -9,32 +9,15 @@
 using namespace std;
 
 
-/* 
-// Deprecated function call.
-int sizefunc(char* commandstream, const char* delim){
-        int i;
-        char* p = strtok(commandstream, delim);
+// Changes userstream to an array to be parsed
+char ** changeToArray(char ** commandList, char* commands, const char* parser);
+//Checks for username and hostname
+void setUsernameHostname();
+// Finds and erases something if needed to be
+void removeComment(string &);
+//Exit program
+void exitProgram(string);
 
-        for (i = 0; p!= NULL; p = strtok(NULL, delim) ){
-                ++i;
-        }
-        return (i+1);
-
-}
-*/
-
-bool checkforexit(char ** &commandList, char* commands, const char* pk){
-        int c = 0;
-        bool exiting = false;
-        char* par = strtok(commands, pk);
-
-        for (c = 0; par != NULL; ++c, par = strtok(NULL, pk)){
-                
-                commandList[c] = par;
-        }
-        return exiting;
-                //cout << "CommandList@" << it << ": " << commandList[it] << endl;        }
-}
 
 char ** changeToArray(char ** commandList, char* commands, const char* parser){
         int it = 0;
@@ -50,74 +33,97 @@ char ** changeToArray(char ** commandList, char* commands, const char* parser){
         return commandList;
 }
 
+void setUsernameHostname(){
+
+    char * username = getlogin();
+    if (username == NULL){
+        perror("getlogin");
+    }
+    char host[100];
+    if (-1 == gethostname(host, 100))
+        perror("gethostname");
+
+    // Prints out the username and hostname if it exists.
+    if (username){
+        cout << username << "@" << host;
+    }
+    cout << "$ ";
+}
+
+void removeComment(string &stream){
+    int hashpos = stream.find('#');
+
+    //Check for comments, and removes anything past #
+    if (hashpos != -1){
+        stream.erase(hashpos);
+    }
+
+}
+
+void exitProgram(string stream){
+    int exitpos = stream.find("exit");
+
+    // Checks for exit word, and exits program if found
+    if (exitpos != -1){
+        exit(0);
+    }
+}
+
 int main()
 {       
         string user_stream;
         char * parsedString = NULL;
-        char * username = getlogin();
 
         const char * parr = " ";
         //const char * andL = "&&";
         //const char * orL =  "||";
         //const char * semiL = ";";
 
-        char host[100];
-        gethostname(host, 100);
+
 
         //Introductory Message to make sure shell is running.
-        cout << "Hello, and welcome to Kevin's shell." << endl;
+        //cout << "Hello, and welcome to Kevin's shell." << endl;
 
         while(1){
+            // Sets Username and Hostname if possible
+            setUsernameHostname();
 
-                // Prints out the username and hostname if it exists.
-                if (username){
-                        cout << username << "@" << host;
+            //Gets line for user stream.
+            getline(cin, user_stream);
+
+            // Removes all the comments before continuing.
+            removeComment(user_stream);
+
+            //Exits program if exit is found.
+            exitProgram(user_stream);
+
+            //Parsing the string using strdup to change from const char* to char*
+            parsedString = strdup(user_stream.c_str());
+
+	        char ** command = new char *[sizeof(parsedString)+1];
+            command = changeToArray(command, parsedString, parr);
+            
+            int pid = fork();
+            if (pid == -1)
+            {
+            	perror("Fork failed");
+	        }
+
+            else if (pid == 0)
+            {
+                if(execvp((const char*) command[0], (char* const*)command) == -1){
+                    perror("Unable to perform request");
                 }
-                cout << "$ ";
+	
+            }
 
-                //Gets line for user stream.
-                getline(cin, user_stream);
-
-                if (user_stream == "exit"){
-                        cout << "Thank you for using the program.\n";
-                        exit(1);
-                }
-                int hashpos = user_stream.find('#');
-                
-                if (hashpos != -1){
-                       user_stream.erase(hashpos);
-                }
-
-                //Parsing the string using strdup to change from const char* to char*
-                parsedString = strdup(user_stream.c_str());
-		        char ** command = new char *[sizeof(parsedString)+1];
-                command = changeToArray(command, parsedString, parr);
-                int pid = fork();
-
-                if (pid == -1){
-                	perror("Fork failed");
-		        }
-                else if (pid == 0){
-			
-                        if(execvp( (const char*) command [0] , (char* const*) command) == -1){
-                                perror("Unable to perform request.");
-                        }
-		
-                }
-                else{
-                        wait(NULL);
-
-                        /*
-                        if (checkforexit(commandstream, parsedString, parr)){
-                                cout << "Thank you for using rshell. Goodbye.\n";
-                                exit(1);
-                        }
-                        */
-                }
-
+            else
+            {
+                if(-1 == wait(NULL))
+                    perror("wait");
+            }
 
         }
-
         return 0;
 
 }	   
